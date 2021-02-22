bc26e7f1f5a3379cb38d0e4aa19049c79784ec5e
Jordan-low <jawdanlow@gmail.com>
2021-02-22 09:27:16 +0800

    error fix

--- Application/Source/NPC.cpp
+++ Application/Source/NPC.cpp
@@ -4,7 +4,7 @@
 NPC::NPC(Scene* scene, NPCTYPE type, std::string name) : Entity(scene, ENTITYTYPE::LIVE_NPC, name) , npcType(npcType) {
 	switch (type) {
 	case TESTNPC:
-		this->associatedNPCMesh = MeshHandler::getMesh(GEO_SEDAN);
+		this->associatedNPCMesh = MeshHandler::getMesh(GEO_NPC);
 		break;
 	default:
 		this->associatedNPCMesh = nullptr;
