b7e2263f5d6d6609cac772f34593fe11252d53af
Big E <56944237+somebodies1@users.noreply.github.com>
2021-02-24 16:43:23 +0800

    Added stuff for item/weapons/cars inventory

--- Application/Source/UIManager.h
+++ Application/Source/UIManager.h
@@ -23,7 +23,7 @@ private:
 	m_height = 720;*/ //screen size
 	ButtonManager* bManagers[UI_MENU_COUNT]; //Init buttons inside this class
 
-	float elapsedTimeinUI;
+	float nextKeyPress;
 	bool uiActive;
 	UI_MENUS currentMenu;
 	bool activeMenus[UI_MENU_COUNT];
