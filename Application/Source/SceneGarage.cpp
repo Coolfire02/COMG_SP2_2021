7c8a53f51cbac5912696cba01c7cb9b022b49834
Jordan-low <jawdanlow@gmail.com>
2021-02-24 14:16:59 +0800

    fixed topdown camera lights

--- Application/Source/SceneGarage.cpp
+++ Application/Source/SceneGarage.cpp
@@ -335,10 +335,10 @@ void SceneGarage::InitLights()
 	light[0].exponent = 1.f;
 	light[0].spotDirection.Set(0.f, 1.f, 0.f);
 
-	light[1].type = Light::LIGHT_POINT;
+	light[1].type = Light::LIGHT_SPOT;
 	light[1].position.set(0, 0, 0);
-	light[1].color.set(1, 0.5, 0);
-	light[1].power = 1.4f;
+	light[1].color.set(1, 0, 1);
+	light[1].power = 0.f;
 	light[1].kC = 1.f;
 	light[1].kL = 0.01f;
 	light[1].kQ = 0.001f;
@@ -603,13 +603,13 @@ void SceneGarage::TopDownMapUpdate(double dt)
 	switch (camera.camType)
 	{
 	case TOPDOWN_FIRSTPERSON:
-		light[1].power = 2.5;
+		light[1].power = 1;
 		light[1].position.set(player->getEntityData()->Translate.x, 1, player->getEntityData()->Translate.z);
 		light[1].spotDirection.Set(-view.x, 0, -view.z);
 		glUniform1f(m_parameters[U_LIGHT1_POWER], light[1].power);
 		break;
 	case TOPDOWN_THIRDPERSON:
-		light[1].power = 2.5;
+		light[1].power = 1;
 		light[1].position.set(player->getEntityData()->Translate.x, 1, player->getEntityData()->Translate.z);
 		light[1].spotDirection.Set(player->getCar()->getEntityData()->Rotation.x * dt, 0, player->getCar()->getEntityData()->Rotation.z * dt);
 		glUniform1f(m_parameters[U_LIGHT1_POWER], light[1].power);
